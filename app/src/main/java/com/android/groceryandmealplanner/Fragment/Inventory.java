806b2e12b998c8e96de0faef3ac92f7d8e161cd4
Faizan Qadri <faizanqadri0220@gmail.com>
2024-12-14 14:05:06 +0530

    Initial commit

---
+++ app/src/main/java/com/android/groceryandmealplanner/Fragment/Inventory.java
@@ -0,0 +1,28 @@
+package com.android.groceryandmealplanner.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.android.groceryandmealplanner.R;
+
+
+public class Inventory extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_inventory, container, false);
+
+
+
+        return view;
+    }
+}
